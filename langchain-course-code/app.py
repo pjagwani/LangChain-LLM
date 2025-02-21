fbdfa013eefbea1a80e5a43759da8054c71acd61
Prakash Jagwani <pjagwani@gmail.com>
2025-02-21 09:18:49 -0500

    Refactor app.py to use langchain_openai and langchain_community imports

--- langchain-course-code/app.py
+++ langchain-course-code/app.py
@@ -1,14 +1,14 @@
 import os 
-import openai 
+import langchain_openai 
 from dotenv import find_dotenv, load_dotenv
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+from langchain_community.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
 
 
 load_dotenv(find_dotenv())
-openai.api_key = os.getenv("OPENAI_API_KEY")
+langchain_openai.api_key = os.getenv("OPENAI_API_KEY")
 
 llm_model = "gpt-3.5-turbo"
 
@@ -21,11 +21,11 @@ llm = OpenAI(temperature=0.7)
 chat_model = ChatOpenAI(temperature=0.7)
 
 
-# print(llm.predict("What is the weather in WA DC"))
-# print("==========")
+#print(llm.predict("What is the weather in WA DC"))
+print("==========")
 
-print(chat_model.predict_messages(messages).content)
-# print(chat_model.predict("What is the weather in WA DC"))
+#print(chat_model.predict_messages(messages).content)
+print(chat_model.predict("What is the weather in WA DC"))
 
 
 
